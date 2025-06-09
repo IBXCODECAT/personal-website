7684d61cd3ac6a20137319375df8711e212ccfed
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2025-06-08 21:38:24 -0500

    Scroll Prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from "react";
 import Blob from "./Components/Blob";
+import Card from "./Components/Card";
+import ScrollPrompt from "./Components/ScrollPrompt";
 
 // Main App Component - This is where we render the Blob and the page content
 export default function App() {
   return (
-    <div className="relative bg-gray-900 min-h-screen text-white flex items-center justify-center font-sans overflow-hidden">
+    <div className="bg-gray-900">
+    <div className="relative  min-h-screen text-white flex items-center justify-center font-sans overflow-hidden">
       
-      <img src="kittyfx.png" alt="site logo" className="z-10" width={450} height={450}/>
+      <img src="kittyfx.png" alt="site logo" className="z-10" width={450} height={450} draggable="false"/>
       {/* The Blob component is rendered here */}
       <Blob />
 
       {/* Example content for the page */}
       <div className="text-center z-10 p-4">
         <h1 className="text-5xl md:text-7xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-emerald-400 to-sky-600">
-          Software Developer
+          Nathan Schmitt
         </h1>
         <p className="text-gray-300 max-w-2xl mx-auto">
           Move your mouse around the screen to see the blob trail effect. The
           blob is a separate component and doesn't interfere with the content
           underneath it.
         </p>
+        
       </div>
+    </div>
+      <ScrollPrompt/>
+      <Card/>
+      <Card/>
     </div>
   );
 }
---
+++ src/Components/Card.tsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return (
+        <div className="bg-slate-300 opacity-15 shadow-md rounded-lg z-10 p-6">
+            <h1>Hello</h1>
+        </div>
+        
+    );
+}
+
+export default Card;
